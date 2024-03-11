2aa7cfd4ba5de0a8d1aab21141b531f3e1cf2ad7
davi-miquelim <davichelimiquelim@gmail.com>
2024-03-11 20:12:18 -0300

    feat: add scanner

--- scanner.py
+++ scanner.py
@@ -10,9 +10,27 @@ class Scanner:
         self.current = 0
         self.line = 1
 
+        self.keywords = {
+            "and": TokenType.AND,
+            "class": TokenType.CLASS,
+            "else": TokenType.ELSE,
+            "false": TokenType.FALSE,
+            "for": TokenType.FOR,
+            "fun": TokenType.FUN,
+            "if": TokenType.IF,
+            "nil": TokenType.NIL,
+            "or": TokenType.OR,
+            "print": TokenType.PRINT,
+            "return": TokenType.RETURN,
+            "super": TokenType.SUPER,
+            "this": TokenType.THIS,
+            "true": TokenType.TRUE,
+            "var": TokenType.VAR,
+            "while": TokenType.WHILE
+        }
 
     def scan_tokens(self):
-        while !(self.is_at_end()):
+        while not self.is_at_end():
             self.start = self.current
             scan_token()
 
@@ -45,14 +63,99 @@ class Scanner:
                 self.add_token(TokenType.SEMICOLON)
             case '*':
                 self.add_token(TokenType.STAR)
+            case '!':
+                self.add_token(TokenType.BANG_EQUAL if self.match('=') else TokenType.BANG)
+            case '=':
+                self.add_token(TokenType.EQUAL_EQUAL if self.match('=') else TokenType.EQUAL)
+            case '<':
+                self.add_token(TokenType.LESS_EQUAL if self.match('=') else TokenType.LESS)
+            case '>':
+                self.add_token(TokenType.GREATER_EQUAL if self.match('=') else TokenType.GREATER)
+            case '/':
+                if self.match('/'):
+                    while self.peek() != "\n" and not self.is_at_end():
+                        self.advance()
+                else:
+                    self.add_token(TokenType.SLASH)
+            case ' ':
+                return
+            case '\r':
+                return
+            case '\t':
+                return
+            case '\n':
+                self.line += 1
+            case '"':
+                self.string()
             case _:
-                Lox.error(self.line, "Unexpected character.")
+                if c.isdigit():
+                    self.number()
+                elif c.isalpha() or c == '_':
+                    self.identifier()
+                else:
+                    Lox.error(self.line, "Unexpected character.")
+
+    def identifier(self):
+        while self.peek().isalnum() or self.peek() == '_':
+            self.advance()
+
+        text = self.source[self.start:self.current]
+        if self.keywords[text]:
+            self.add_token(self.keywords[text])
+        else:
+            self.add_token(TokenType.IDENTIFIER)
+
+    def number(self):
+        while self.peek().isdigit():
+            self.advance()
+
+        if self.peek() == "." and self.peek_next().isdigit():
+            self.advance()
+            while self.peek().isdigit():
+                self.advance()
+
+        self.add_token(TokenType.NUMBER, float(self.source[self.start:self.current])) 
+
+    def string(self):
+        while self.peek() != '"' and not self.is_at_end():
+            if self.peek == "\n":
+                self.line += 1
+
+            self.advance()
+
+        if self.is_at_end():
+            Lox.error(self.line, "Unterminated string.")
+
+        self.advance()
+
+        # Trim the surounding quotes
+        value = self.source[self.start + 1: self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def match(self, expected): 
+        if self.is_at_end() or self.source[current] != expected:
+            return False
+
+        self.current += 1
+        return True
+
+    def peek(self):
+        if self.is_at_end():
+            return '\0'
+
+        return self.source[self.current]
+
+    def peek_next(self):
+        if self.current + 1 >= len(self.source):
+            return '\0'
+
+        return self.source[self.current + 1]
 
     def advance(self):
         self.current += 1
         return self.source[self.current]
 
     def add_token(self, token_type, literal = Null):
-        text = self.source.substring[self.start, self.current]
+        text = self.source[self.start:self.current]
         self.tokens.push(Token(token_type, text, literal, self.line)) 
 
